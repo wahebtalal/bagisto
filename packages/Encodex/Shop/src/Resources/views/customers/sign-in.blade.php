64528cd7041144abf50243e7b7af37ab09f8a926
Wahieb <wahebtalal@gmail.com>
2024-08-15 23:32:51 +0300

    sign in shadow

--- packages/Encodex/Shop/src/Resources/views/customers/sign-in.blade.php
+++ packages/Encodex/Shop/src/Resources/views/customers/sign-in.blade.php
@@ -37,7 +37,7 @@
         {!! view_render_event('bagisto.shop.customers.login.logo.after') !!}
 
         <!-- Form Container -->
-        <div class="m-auto w-full max-w-[870px] rounded-xl border border-zinc-200 p-16 px-[90px] max-md:px-8 max-md:py-8 max-sm:border-none max-sm:p-0 shadow-2xl hover:shadow-gray-500 transition">
+        <div class="m-auto w-full max-w-[870px] rounded-xl border border-zinc-200 p-16 px-[90px] max-md:px-8 max-md:py-8 max-sm:border-none max-sm:p-0 shadow-2xl hover:shadow-gray-500 transition hover:scale-[1.01] ">
             <h1 class="font-dmserif text-4xl max-md:text-3xl max-sm:text-xl">
                 @lang('shop::app.customers.login-form.page-title')
             </h1>
